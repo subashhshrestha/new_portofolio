7c7545536e76fa21827fbff626325658cad246d1
Subash Shrestha <subashhshrestha10@gmail.com>
2019-08-17 11:58:38 +0545

    Added skill and progressBar

--- app.js
+++ app.js
@@ -108,18 +108,119 @@ $(document).ready(function () {
 
             }
 
-            if (skill - windowsscroll <= 100 && flag == 1) {
+            if (skill - windowsscroll <= 150 && flag == 1) {
                 flag = 0;
-                width = 0;
+                jswidth = 0;
                 let javascript = $("#javascript");
-                let per = setInterval(() => {
-                    width = width + 1;
-                    javascript.css({ "width": `${width}%` });
-                    javascript.html(`${width}%`)
-                    if(width == 70){
-                        clearInterval(per)
+                let jsinterval = setInterval(() => {
+                    jswidth = jswidth + 1;
+                    javascript.css({ "width": `${jswidth}%` });
+                    javascript.html(`${jswidth}%`)
+                    if(jswidth == 75){
+                        clearInterval(jsinterval)
                     }
-                }, 100);
+                }, 50);
+
+                ngwidth = 0;
+                let angular = $("#angular");
+                let nginterval = setInterval(() => {
+                    ngwidth = ngwidth + 1;
+                    angular.css({ "width": `${ngwidth}%` });
+                    angular.html(`${ngwidth}%`)
+                    if(ngwidth == 80){
+                        clearInterval(nginterval)
+                    }
+                }, 50);
+
+                reactwidth = 0;
+                let react = $("#react");
+                let reactinterval = setInterval(() => {
+                    reactwidth = reactwidth + 1;
+                    react.css({ "width": `${reactwidth}%` });
+                    react.html(`${reactwidth}%`)
+                    if(ngwidth == 53){
+                        clearInterval(reactinterval)
+                    }
+                }, 50);
+
+                htmlwidth = 0;
+                let html = $("#html");
+                let htmlinterval = setInterval(() => {
+                    htmlwidth = htmlwidth + 1;
+                    html.css({ "width": `${htmlwidth}%` });
+                    html.html(`${htmlwidth}%`)
+                    if(htmlwidth == 79){
+                        clearInterval(htmlinterval)
+                    }
+                }, 50);
+
+                csswidth = 0;
+                let css = $("#css");
+                let cssinterval = setInterval(() => {
+                    csswidth = csswidth + 1;
+                    css.css({ "width": `${csswidth}%` });
+                    css.html(`${csswidth}%`)
+                    if(csswidth == 69){
+                        clearInterval(cssinterval)
+                    }
+                }, 50);
+
+                nodewidth = 0;
+                let node = $("#node");
+                let nodeinterval = setInterval(() => {
+                    nodewidth = nodewidth + 1;
+                    node.css({ "width": `${nodewidth}%` });
+                    node.html(`${nodewidth}%`)
+                    if(nodewidth == 50){
+                        clearInterval(nodeinterval)
+                    }
+                }, 50);
+
+                javawidth = 0;
+                let java = $("#java");
+                let javainterval = setInterval(() => {
+                    javawidth = javawidth + 1;
+                    java.css({ "width": `${javawidth}%` });
+                    java.html(`${javawidth}%`)
+                    if(javawidth == 43){
+                        clearInterval(javainterval)
+                    }
+                }, 50);
+
+                pythonwidth = 0;
+                let python = $("#python");
+                let pythoninterval = setInterval(() => {
+                    pythonwidth = pythonwidth + 1;
+                    python.css({ "width": `${pythonwidth}%` });
+                    python.html(`${pythonwidth}%`)
+                    if(pythonwidth == 40){
+                        clearInterval(pythoninterval)
+                    }
+                }, 50);
+
+                gitwidth = 0;
+                let git = $("#git");
+                let gitinterval = setInterval(() => {
+                    gitwidth = gitwidth + 1;
+                    git.css({ "width": `${gitwidth}%` });
+                    git.html(`${gitwidth}%`)
+                    if(gitwidth == 40){
+                        clearInterval(gitinterval)
+                    }
+                }, 50);
+
+                mysqlwidth = 0;
+                let mysql = $("#mysql");
+                let mysqlinterval = setInterval(() => {
+                    mysqlwidth = mysqlwidth + 1;
+                    mysql.css({ "width": `${mysqlwidth}%` });
+                    mysql.html(`${mysqlwidth}%`)
+                    if(mysqlwidth == 60){
+                        clearInterval(mysqlinterval)
+                    }
+                }, 50);
+
+
                 
 
             }
